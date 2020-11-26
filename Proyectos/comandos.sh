1f5ce81e1fa26bf3eecded1e844a4983a08c13a0
neapovea <neapovea@gmail.com>
2020-11-26 11:41:02 +0100

    Nuevos ficheros de gitignore y de comandos docker

---
+++ Proyectos/comandos.sh
@@ -0,0 +1,7 @@
+
+
+#Generar imagen con nombre neapovea/simpleweb
+sudo docker build -t neapovea/simpleweb .
+
+#Arrancar docker basado en el imagen indicando puerto
+sudo docker run -p 8080:8080 neapovea/simpleweb
