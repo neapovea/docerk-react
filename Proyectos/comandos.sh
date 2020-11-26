93c4f68174d7bb1ebaea7372bc12d18f71b02f98
neapovea <neapovea@gmail.com>
2020-11-26 18:48:50 +0100

    57_container-maintenance_visits

--- Proyectos/comandos.sh
+++ Proyectos/comandos.sh
@@ -1,13 +1,18 @@
 
+## Git
+
 #comandos git para agregar código del curso
-git flow feature start 54_networking-docker-compose_visits
+git flow feature start 57_container-maintenance_visits
 git add .
-git commit -m "54_networking-docker-compose_visits"
-git flow feature finish 54_networking-docker-compose_visits
+git commit -m "57_container-maintenance_visits"
+git flow feature finish 57_container-maintenance_visits
 
-git flow release start 1.0.54.1
-git flow release finish -m "networking-docker-compose_visits" 1.0.54.1
+git flow release start 1.0.57.1
+git flow release finish -m "container-maintenance" 1.0.57.1
 
+git push --tags origin master develop
+
+## Docker
 
 #Generar imagen con nombre neapovea/simpleweb
 sudo docker build -t neapovea/simpleweb .
@@ -21,3 +26,14 @@ sudo docker run -p  8080:8080 neapovea/simpleweb sh
 
 #ejecutar sh dentro de unc ontendor ya lanzado
 sudo docker exec -it CONTANER_ID sh
+
+
+## Docker Compose
+sudo docker-compose up #ejecutar contenedor
+sudo docker-compose up --build #generar y ejecutar contenedor
+sudo docker-compose up -d #ejecutar docker en background
+
+
+sudo docker-compose down
+sudo docker-compse
+
