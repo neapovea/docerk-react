3f2b8a8d7179dc2c29e6af11cc68234a8f1c32df
neapovea <neapovea@gmail.com>
2020-11-26 11:58:06 +0100

    inclusiónd de comandos git.

--- Proyectos/comandos.sh
+++ Proyectos/comandos.sh
@@ -1,7 +1,23 @@
 
+#comandos git para agregar código del curso
+git flow feature start 48_minimizing-cache-busting_simpleweb
+git add .
+git commit -m "48_minimizing-cache-busting_simpleweb"
+git flow feature finish 48_minimizing-cache-busting_simpleweb
+
+git flow release start 1.0.48.1
+git flow release finish -m "minimizing-cache-busting_simpleweb" 1.0.48.1
+
 
 #Generar imagen con nombre neapovea/simpleweb
 sudo docker build -t neapovea/simpleweb .
 
 #Arrancar docker basado en el imagen indicando puerto
 sudo docker run -p 8080:8080 neapovea/simpleweb
+
+#lanza contendor y lanza sh dentor del mismo
+sudo docker run -p  8080:8080 neapovea/simpleweb sh
+
+
+#ejecutar sh dentro de unc ontendor ya lanzado
+sudo docker exec -it CONTANER_ID sh
