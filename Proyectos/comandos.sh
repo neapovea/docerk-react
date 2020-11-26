72e1909f74375d985b690ea629cde7f0460e146f
neapovea <neapovea@gmail.com>
2020-11-26 18:33:16 +0100

    54_networking-docker-compose_visits

--- Proyectos/comandos.sh
+++ Proyectos/comandos.sh
@@ -1,12 +1,12 @@
 
 #comandos git para agregar código del curso
-git flow feature start 53_docker-compose-files_visits
+git flow feature start 54_networking-docker-compose_visits
 git add .
-git commit -m "53_docker-compose-files_visits"
-git flow feature finish 53_docker-compose-files_visits
+git commit -m "54_networking-docker-compose_visits"
+git flow feature finish 54_networking-docker-compose_visits
 
-git flow release start 1.0.53.1
-git flow release finish -m "docker-compose-files_visits" 1.0.53.1
+git flow release start 1.0.54.1
+git flow release finish -m "networking-docker-compose_visits" 1.0.54.1
 
 
 #Generar imagen con nombre neapovea/simpleweb
